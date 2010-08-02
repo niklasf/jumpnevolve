42d8dbfba1577faad815c71dd501c276c198f53c
niklas.fiekas <niklas.fiekas@42192f38-024e-0d60-f621-68f43529c6e9>
2010-08-02 17:17:43 +0000

    ResourceManager: JavaDoc, Sounds laden, Warteliste aufräumen
    git-svn-id: https://jumpnevolve.googlecode.com/svn/trunk/jumpnevolve@44 42192f38-024e-0d60-f621-68f43529c6e9

--- src/com/googlecode/jumpnevolve/graphics/ResourceManager.java
+++ src/com/googlecode/jumpnevolve/graphics/ResourceManager.java
@@ -17,11 +17,13 @@
 
 package com.googlecode.jumpnevolve.graphics;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 
 /**
  * @author niklas
@@ -35,6 +37,8 @@ public class ResourceManager {
 	
 	private HashMap<String, Image> images = new HashMap<String, Image>();
 	
+	private HashMap<String, Sound> sounds = new HashMap<String, Sound>();
+	
 	private ResourceManager() {
 		
 	}
@@ -47,28 +51,44 @@ public class ResourceManager {
 		}
 	}
 	
+	/**
+	 * Fügt Ressourcen in die Wartschleife ein, sodass sie geladen werden,
+	 * sobald die Möglichkeit besteht.
+	 * 
+	 * @param identifier Pfad zur Ressource.
+	 */
 	public void schedule(String identifier) {
 		identifier = normalizeIdentifier(identifier);
-		if(!this.schedule.contains(identifier) && !this.images.containsKey(identifier)) {
+		if(!this.schedule.contains(identifier) && !this.images.containsKey(identifier) && !this.sounds.containsKey(identifier)) {
 			this.schedule.add(identifier);
 		}
 	}
 	
 	private void load(String identifier) {
 		try {
-			if(identifier.endsWith(".png")) {
+			if(identifier.endsWith(".png")) { // Bild laden
 				this.images.put(identifier, new Image(identifier));
-			} else {
+			} else if(identifier.endsWith(".png?reverse")) { // Umgedrehted Bild laden
+				this.images.put(identifier, getImage(identifier.substring(0, identifier.indexOf('?'))).getFlippedCopy(true, false));
+			} else if(identifier.endsWith(".ogg")) { // Sound laden
+				this.sounds.put(identifier, new Sound(identifier));
+			} else { // Ressourcentyp unbekannt
 				throw new ResourceError("Unkown ressource type: " + identifier);
 			}
-			// this.schedule.remove(identifier);
 		} catch(SlickException e) {
 			throw new ResourceError(e);
 		}
 	}
 	
+	/**
+	 * Läd die gewünschte Bildressource vom Dateisystem oder aus dem Cache.
+	 * 
+	 * @param id Pfad zur Bildressource.
+	 * @return Das Bild.
+	 */
 	public Image getImage(String id) {
 		id = normalizeIdentifier(id);
+		
 		Image image = this.images.get(id);
 		if(image == null) {
 			load(id);
@@ -78,24 +98,68 @@ public class ResourceManager {
 		}
 	}
 	
+	/**
+	 * Erzeugt ein Bild das in X-Richtung umgedreht wurde oder holt es aus dem
+	 * Cache.
+	 * 
+	 * @param id Pfad zur Bildressource.
+	 * @return Das umgedrehte Bild.
+	 */
 	public Image getRevertedImage(String id) {
 		id = normalizeIdentifier(id);
+		
 		Image image = this.images.get(id + "?reverse");
 		if(image == null) {
-			Image reverted = getImage(id).getFlippedCopy(true, false);
-			this.images.put(id + "?reverse", reverted);
-			return reverted;
+			load(id + "?reverse");
+			return this.images.get(id + "?reverse");
 		} else {
 			return image;
 		}
 	}
 	
-	public void load() {
-		for(String id: this.schedule) {
+	/**
+	 * Läd eine Sounddatei vom Dateisystem oder aus dem Cache.
+	 * 
+	 * @param id Pfad zur Soundresource.
+	 * @return Die Sounddatei.
+	 */
+	public Sound getSound(String id) {
+		id = normalizeIdentifier(id);
+		
+		Sound sound = this.sounds.get(id);
+		if(sound == null) {
 			load(id);
+			return this.sounds.get(id);
+		} else {
+			return sound;
 		}
 	}
 	
+	/**
+	 * Arbeitet die Liste aller angeforderten Ressourcen ab und versucht sie zu
+	 * laden.
+	 */
+	public void load() {
+		ArrayList<String> loaded = new ArrayList<String>(this.schedule.size());
+		
+		// Ressourcen laden
+		try {
+			for(String id: this.schedule) {
+				load(id);
+				loaded.add(id);
+			}
+		} finally {
+			
+			// Aus der Warteschleife löschen
+			for(String id: loaded) {
+				this.schedule.remove(id);
+			}
+		}
+	}
+	
+	/**
+	 * @return Der von der gesamten Anwendung geteilte ResourceManager.
+	 */
 	public static ResourceManager getInstance() {
 		if(instance == null) {
 			instance = new ResourceManager();
