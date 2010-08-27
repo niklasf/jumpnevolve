df7d4a37d1812cd06363b2ff565ddabd3e8a1dbf
niklas.fiekas <niklas.fiekas@42192f38-024e-0d60-f621-68f43529c6e9>
2010-08-27 17:28:19 +0000

    Änderungsvorschlag für die Levelloader Struktur
    git-svn-id: https://jumpnevolve.googlecode.com/svn/trunk/jumpnevolve@92 42192f38-024e-0d60-f621-68f43529c6e9

--- src/com/googlecode/jumpnevolve/game/Levelloader.java
+++ src/com/googlecode/jumpnevolve/game/Levelloader.java
@@ -6,16 +6,27 @@ package com.googlecode.jumpnevolve.game;
  * @author Erik Wagner
  * 
  */
-public class Levelloader {
+public class Levelloader implements Runnable {
+	
+	private String source;
+	
+	private Level level;
+	
+	public Levelloader(String source) {
+		this.source = source;
+	}
 
-	/**
-	 * Lädt ein Level und gibt dieses zurück.
-	 * 
-	 * @param source
-	 *            Die Datei, aus dem das Level geladen wird
-	 * @return Das geladene Level
-	 */
-	public static Level loadLevel(String source) {
-		return new Level(0, 0, 0);
+	public void run() {
+		// Level laden und in this.level speichern
+	}
+	
+	public Level getLevel() {
+		return this.level;
+	}
+	
+	public static Level asyncloadLevel(String source) {
+		Levelloader loader = new Levelloader(source);
+		loader.run();
+		return loader.getLevel();
 	}
 }
