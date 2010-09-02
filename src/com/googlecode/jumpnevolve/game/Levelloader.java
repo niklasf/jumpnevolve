7ed98a7e4fc02e7180bc19957654faf0a7bf4886
niklas.fiekas <niklas.fiekas@42192f38-024e-0d60-f621-68f43529c6e9>
2010-09-02 20:49:11 +0000

    TODOS und kleine Korrekturen für den Levelloader
    git-svn-id: https://jumpnevolve.googlecode.com/svn/trunk/jumpnevolve@101 42192f38-024e-0d60-f621-68f43529c6e9

--- src/com/googlecode/jumpnevolve/game/Levelloader.java
+++ src/com/googlecode/jumpnevolve/game/Levelloader.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import org.newdawn.slick.util.Log;
+
 import com.googlecode.jumpnevolve.game.objects.*;
 import com.googlecode.jumpnevolve.graphics.world.AbstractObject;
 import com.googlecode.jumpnevolve.math.Vector;
@@ -33,110 +35,113 @@ public class Levelloader {
 	}
 
 	public void run() {
+		FileInputStream levelFile = null;
+		
 		try {
-			FileInputStream levelFile;
-			levelFile = new FileInputStream(source);
-			String[] sourceSplit = this.source.split(".");
-			if (sourceSplit.length == 2) {
-				if (sourceSplit[1].equals("txt")) {
-					// neues Level aus Textdatei erstellen
-					BufferedReader levelFileReader = new BufferedReader(
-							new InputStreamReader(levelFile));
-					// Level durch Kopfzeile erstellen --> Größenordnungen
-					String firstLine = levelFileReader.readLine();
-					String[] firstLineSplit = firstLine.split("_");
-					if (firstLineSplit[0].equals("Leveldimensionen") == false) {
-						// FIXME: Fehlermeldung ausgeben und abbrechen
-					}
-					this.level = new Level(Integer.parseInt(firstLineSplit[1]),
-							Integer.parseInt(firstLineSplit[2]), Integer
-									.parseInt(firstLineSplit[3]));
-					// Einstellungen vornehmen
-					String secondLine = levelFileReader.readLine();
-					String[] secondLineSplit = secondLine.split("_");
-					if (firstLineSplit[0].equals("Leveleinstellungen") == false) {
-						// FIXME: Fehlermeldung ausgeben und abbrechen
-					}
-					// FIXME: Einstellungen für das Level vornehmen
-					// Beispiele: verfügbare Charaktere, Timer etc.
+			levelFile = new FileInputStream(this.source);
+			
+			if(this.source.toLowerCase().endsWith(".txt")) {
+				// Neues Level aus Textdatei erstellen
+				BufferedReader levelFileReader = new BufferedReader(
+						new InputStreamReader(levelFile));
+				
+				// Level durch Kopfzeile erstellen --> Größenordnungen
+				String firstLine = levelFileReader.readLine();
+				String[] firstLineSplit = firstLine.split("_");
+				if (firstLineSplit[0].equals("Leveldimensionen") == false) {
+					// FIXME: Fehlermeldung ausgeben und abbrechen
+				}
+				this.level = new Level(Integer.parseInt(firstLineSplit[1]),
+						Integer.parseInt(firstLineSplit[2]), Integer
+								.parseInt(firstLineSplit[3]));
+				// Einstellungen vornehmen
+				String secondLine = levelFileReader.readLine();
+				String[] secondLineSplit = secondLine.split("_");
+				if (firstLineSplit[0].equals("Leveleinstellungen") == false) {
+					// FIXME: Fehlermeldung ausgeben und abbrechen
+				}
+				// FIXME: Einstellungen für das Level vornehmen
+				// Beispiele: verfügbare Charaktere, Timer etc.
 
-					// HashMaps für Objekte zum Zwischenspeichern erstellen
-					HashMap<String, AbstractObject> activableObjects = new HashMap<String, AbstractObject>();
-					ArrayList<AbstractObject> activatingObjects = new ArrayList<AbstractObject>();
-					ArrayList<String[]> argumtensForActivating = new ArrayList<String>();
-					ArrayList<AbstractObject> otherObjects = new ArrayList<AbstractObject>();
+				// HashMaps für Objekte zum Zwischenspeichern erstellen
+				HashMap<String, AbstractObject> activableObjects = new HashMap<String, AbstractObject>();
+				ArrayList<AbstractObject> activatingObjects = new ArrayList<AbstractObject>();
+				ArrayList<String[]> argumtensForActivating = new ArrayList<String[]>();
+				ArrayList<AbstractObject> otherObjects = new ArrayList<AbstractObject>();
 
-					String current = levelFileReader.readLine();
-					while (current != null) { // Pseudo-Methode ersetzen
-						String[] currentSplit = current.split("_");
-						String[] currentArguments = currentSplit[2].split(",");
-						current = levelFileReader.readLine();
-						if (currentSplit[0].equals("WalkingSoldier")) {
-							otherObjects.add(new WalkingSoldier(this.level,
-									this.toVector(currentSplit[1])));
-						} else if (currentSplit[0].equals("JumpingSoldier")) {
-							otherObjects.add(new JumpingSoldier(this.level,
-									this.toVector(currentSplit[1])));
-						} else if (currentSplit[0].equals("Soldier")) {
-							otherObjects.add(new Soldier(this.level, this
-									.toVector(currentSplit[1])));
-						} else if (currentSplit[0].equals("KillingMachine")) {
-							otherObjects.add(new KillingMachine(this.level,
-									this.toVector(currentSplit[1])));
-						} else if (currentSplit[0].equals("Button")) {
-							activatingObjects.add(new Button(this.level, this
-									.toVector(currentSplit[1]), this
-									.toFloat(currentArguments[0])));
-							String[] argument = new String[currentArguments.length - 1];
-							for (int i = 1; i < currentArguments.length; i++) {
-								argument[i - 1] = currentArguments[i];
-							}
-							argumtensForActivating.add(argument);
-						} else if (currentSplit[0].equals("Door")) {
-							activableObjects
-									.put(currentArguments[0], new Door(
-											this.level, this
-													.toVector(currentSplit[1])));
-						} else if (currentSplit[0].equals("Ground")) {
-							otherObjects.add(new Ground(this.level, this
-									.toVector(currentSplit[1]), this
-									.toVector(currentArguments[0])));
-						} else if (currentSplit[0].equals("RollingBall")) {
-							otherObjects.add(new RollingBall(this.level, this
-									.toVector(currentSplit[1])));
+				String current = levelFileReader.readLine();
+				while (current != null) { // Pseudo-Methode ersetzen
+					String[] currentSplit = current.split("_");
+					String[] currentArguments = currentSplit[2].split(",");
+					current = levelFileReader.readLine();
+					if (currentSplit[0].equals("WalkingSoldier")) {
+						otherObjects.add(new WalkingSoldier(this.level,
+								this.toVector(currentSplit[1])));
+					} else if (currentSplit[0].equals("JumpingSoldier")) {
+						otherObjects.add(new JumpingSoldier(this.level,
+								this.toVector(currentSplit[1])));
+					} else if (currentSplit[0].equals("Soldier")) {
+						otherObjects.add(new Soldier(this.level, this
+								.toVector(currentSplit[1])));
+					} else if (currentSplit[0].equals("KillingMachine")) {
+						otherObjects.add(new KillingMachine(this.level,
+								this.toVector(currentSplit[1])));
+					} else if (currentSplit[0].equals("Button")) {
+						activatingObjects.add(new Button(this.level, this
+								.toVector(currentSplit[1]), this
+								.toFloat(currentArguments[0])));
+						String[] argument = new String[currentArguments.length - 1];
+						for (int i = 1; i < currentArguments.length; i++) {
+							argument[i - 1] = currentArguments[i];
 						}
-						// TODO: Weitere Klassen einfügen
-						// Aktivierungszuweisungen vollziehen
+						argumtensForActivating.add(argument);
+					} else if (currentSplit[0].equals("Door")) {
+						activableObjects
+								.put(currentArguments[0], new Door(
+										this.level, this
+												.toVector(currentSplit[1])));
+					} else if (currentSplit[0].equals("Ground")) {
+						otherObjects.add(new Ground(this.level, this
+								.toVector(currentSplit[1]), this
+								.toVector(currentArguments[0])));
+					} else if (currentSplit[0].equals("RollingBall")) {
+						otherObjects.add(new RollingBall(this.level, this
+								.toVector(currentSplit[1])));
 					}
-				} else if (sourceSplit[1].equals("bin")) {
-					// Speicherung laden --> Level-Objekt
-					ObjectInputStream objectLevelFile = new ObjectInputStream(
-							levelFile);
-					// Objekt lesen, in Level konvertieren und abspeichern
-					try {
-						Object object = objectLevelFile.readObject();
-						if (object instanceof Level) {
-							this.level = (Level) object;
-						}
-					} catch (ClassNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+					// TODO: Weitere Klassen einfügen
+					// Aktivierungszuweisungen vollziehen
+				}
+			} else if(this.source.toLowerCase().endsWith(".bin")) {
+				
+				// Speicherung laden --> Level-Objekt
+				ObjectInputStream objectLevelFile = new ObjectInputStream(
+						levelFile);
+				// Objekt lesen, in Level konvertieren und abspeichern
+				try {
+					Object object = objectLevelFile.readObject();
+					if (object instanceof Level) {
+						this.level = (Level) object;
 					}
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 			} else {
 				// Ausgabe: Fehler beim Laden des Levels
 				// FIXME: Möglich durch throw new IOException()?
+				// FIXME: try catch finally throw etwas überarbeiten.
 			}
 		} catch (IOException e) {
 			// Ausgabe: Fehler beim Laden des Levels
 		} catch (DataInputException e) {
 
 		} finally {
-			try {
-				levelFile.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			if(levelFile != null) {
+				try {
+					levelFile.close();
+				} catch (IOException e) {
+					Log.error(e);
+				}
 			}
 		}
 	}
@@ -152,21 +157,31 @@ public class Levelloader {
 	}
 
 	private Vector toVector(String koordinate) throws DataInputException {
-		String[] koordinates = koordinate.split("|");
-		// FIXME: Falscher String (nicht in der Form "50|50") abfangen und
-		// Fehler melden
-		return new Vector(Float.parseFloat(koordinates[0]), Float
-				.parseFloat(koordinates[1]));
+		// TODO: Vector.parseVector könnte auch direkt verwendet werden.
+		try {
+			return Vector.parseVector(koordinate);
+		} catch(NumberFormatException e) {
+			throw new DataInputException(koordinate);
+		}
 	}
 
 	private float toFloat(String argument) throws DataInputException {
-		// FIXME: Falscher String (nicht in der Form "5.5") abfangen und
-		// Fehler melden
-		return Float.parseFloat(argument);
+		// TODO: Eigentlich könnte man auch direkt NumberFormatException
+		// werfen bzw. nicht extra abfangen
+		// bzw. Float.parseFloat direkt im Code verwenden.
+		try {
+			return Float.parseFloat(argument);
+		} catch(NumberFormatException e) {
+			throw new DataInputException(argument);
+		}
 	}
 }
 
+// TODO: Wenn diese Klasse weiter nützlich ist, sollte sie eine
+// eigene Klassendatei bekommen.
 class DataInputException extends Exception {
+
+	private static final long serialVersionUID = 8875770146455595015L;
 
 	private final String wrongInput;
 
