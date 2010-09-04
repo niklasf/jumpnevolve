4126013a77bf13e94c82ba40b5b49fec25458443
niklas.fiekas <niklas.fiekas@42192f38-024e-0d60-f621-68f43529c6e9>
2010-09-04 14:53:34 +0000

    Einfachster denkbarer Enginetestfall geschrieben
    git-svn-id: https://jumpnevolve.googlecode.com/svn/trunk/jumpnevolve@112 42192f38-024e-0d60-f621-68f43529c6e9

---
+++ tests/com/googlecode/jumpnevolve/tests/graphics/EmptyWorldTest.java
@@ -0,0 +1,30 @@
+package com.googlecode.jumpnevolve.tests.graphics;
+
+import com.googlecode.jumpnevolve.graphics.Engine;
+import com.googlecode.jumpnevolve.graphics.world.World;
+
+/**
+ * <p>
+ * Testfall für die Grafikengine.
+ * <p>
+ * 
+ * <p>
+ * Erwartetes Verhalten ist das Anzeigen eines leeren Fensters.
+ * </p>
+ * 
+ * @author Niklas Fiekas
+ */
+public class EmptyWorldTest {
+
+	/**
+	 * @param args Kommandozeilenargumente werden ignoriert.
+	 */
+	public static void main(String[] args) {
+		// Neue, leere Welt erzeugen
+		Engine.getInstance().addState(new World(0, 0, 0));
+		
+		// Engine starten
+		Engine.getInstance().start();
+	}
+
+}
