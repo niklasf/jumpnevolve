b03d13518c0c33812b7b354aa06b8ada9aa32c02
niklas.fiekas <niklas.fiekas@42192f38-024e-0d60-f621-68f43529c6e9>
2010-09-04 15:00:46 +0000

    Testfall für die Physikengine
    git-svn-id: https://jumpnevolve.googlecode.com/svn/trunk/jumpnevolve@113 42192f38-024e-0d60-f621-68f43529c6e9

---
+++ tests/com/googlecode/jumpnevolve/tests/graphics/GravityTest.java
@@ -0,0 +1,38 @@
+package com.googlecode.jumpnevolve.tests.graphics;
+
+import com.googlecode.jumpnevolve.game.objects.Ground;
+import com.googlecode.jumpnevolve.game.objects.RollingBall;
+import com.googlecode.jumpnevolve.graphics.Engine;
+import com.googlecode.jumpnevolve.graphics.world.World;
+import com.googlecode.jumpnevolve.math.Vector;
+
+/**
+ * <p>
+ * Einfacher Testfall für die Physikengine.
+ * <p>
+ * 
+ * <p>
+ * Erwartetes Verhalten:
+ * Die Spielfigur fällt nach unten auf den Boden und bleibt dort liegen.
+ * </p>
+ * 
+ * @author Niklas Fiekas
+ */
+public class GravityTest {
+
+	/**
+	 * @param args Kommandozeilenargumente werden ignoriert.
+	 */
+	public static void main(String[] args) {
+		Engine engine = Engine.getInstance();
+		
+		World world = new World(10, 10, 1);
+		
+		world.add(new Ground(world, new Vector(1.5f, 2.0f), new Vector(1.0f, 0.5f)));
+		world.add(new RollingBall(world, new Vector(1.5f, 1.0f)));
+		
+		engine.switchState(world);
+		engine.start();
+	}
+
+}
